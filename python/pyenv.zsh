418b8aabbb08faeb395e368c5a28ba8ce096faee
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-02-23 13:40:16 -0600

    moved pyenv stuff to system to make sure the pathing stuff works

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -4,7 +4,4 @@ export WORKON_HOME=$HOME/.virtualenvs
 
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 
-eval "$(pyenv init -)"
-
-pyenv virtualenvwrapper
-
+# See system/_path for the setting for the remainder of the pyenv shim
