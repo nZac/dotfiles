1cdf612f3efc34ec0c77a8780c5f762615a98286
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-08-01 22:41:55 -0400

    Remove random line

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -3,5 +3,3 @@ export PYENV_ROOT="$HOME/.pyenv"
 export WORKON_HOME=$HOME/.virtualenvs
 
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-
-# See system/_path for the setting for the remainder of the pyenv shim
