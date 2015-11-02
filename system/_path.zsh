c941049819cf10177c7e79c0f8e9185a3d912b4e
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-11-02 10:51:19 -0500

    Update path'

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:./node_modules/.bin"
+export PATH="$PATH:/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:./node_modules/.bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 
